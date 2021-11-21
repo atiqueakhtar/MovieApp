82c20d95bccc5cff7390c7c54ce5d34549417bb3
Atique Akhtar <atiqueakhtar786@gmail.com>
2021-11-21 20:29:17 +0530

    heroku deployment changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const path = require('path')
+const port = process.env.PORT || 5000;
 const moviesData = {
     romance: [{
             name: "Twilight",
@@ -142,4 +143,10 @@ app.post('/genre-specific', (req, res) => {
     res.send(movieList);
 })
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 app.listen(port, () => console.log(`server started at port ${port}`));
